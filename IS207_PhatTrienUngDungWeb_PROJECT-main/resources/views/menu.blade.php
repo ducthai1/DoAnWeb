ecf859506eb8b3d391b206554e423db59fc21e06
Thái Tăng Đức <ducthai1>
2022-12-04 22:05:26 +0700

    code

---
+++ IS207_PhatTrienUngDungWeb_PROJECT-main/resources/views/menu.blade.php
@@ -0,0 +1,22 @@
+@extends('main')
+
+@section('content')
+<div class="bg0 m-t-23 p-b-140">
+    <div class="container">
+        <div class="flex-w flex-sb-m p-b-52">
+            <div class="flex-w flex-l-m filter-tope-group m-tb-10">
+                <h1>{{ $title }}</h1>
+            </div>
+
+            @include('products.list')
+
+        <!-- Load more
+        <div class="flex-c-m flex-w w-full p-t-45">
+            <a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
+                Load More
+            </a>
+        </div> -->
+    </div>
+</div>
+
+@endsection
